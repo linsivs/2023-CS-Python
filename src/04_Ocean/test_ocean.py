d995228ca1d60e8c0e02f90cc8059376d0bf060c
alin <linsivs@mail.ru>
2023-12-28 11:36:21 +1000

    Update test_ocean.py

--- src/04_Ocean/test_ocean.py
+++ src/04_Ocean/test_ocean.py
@@ -2,13 +2,21 @@ from ocean import Ocean
 
 
 def test_ocean_init():
-    Ocean
-    assert False
+    init_state = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    ocean = Ocean(init_state = init_state)
+    assert ocean.state == init_state
 
 
 def test_ocean_repr():
-    assert False
+    init_state = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    ocean = Ocean(init_state = init_state)
+    expected_repr = "Ocean([[1, 2, 3], [4, 5, 6], [7, 8, 9]])"
+    assert repr(ocean) == expected_repr
 
 
 def test_ocean_step():
-    assert False
+    init_state = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    ocean = Ocean(init_state = init_state)
+    next_state = ocean.gen_next_quantum()
+    expected_next_state = [[2, 3, 4], [5, 6, 7], [8, 9, 10]]
+    assert next_state.state == expected_next_state
